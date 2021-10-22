df922671339b6ed4bee142a96ae47f643f808d4c
末日沙兔 <2657487207@qq.com>
2021-10-22 12:08:38 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,15 +5,5 @@ import App from './App.vue'
 import '@/assets/font/HarmonyOS_Sans_SC/font.css'
 import '@/assets/font/UIcons/font.css'
 
-import router from './router'
 
-// store 
-import store from './store'
-
-import CScrollbar from 'c-scrollbar';
-
-import VueClipboard from 'vue-clipboard2'
-
-
-
-createApp(App).use(router).use(store).use(CScrollbar).use(VueClipboard).mount('#app')
+createApp(App).mount('#app')
